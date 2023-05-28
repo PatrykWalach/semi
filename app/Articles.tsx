70ce624c336c69cf5ba2f937b42048616c11c4ec
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-05-28 16:35:22 +0200

    fix: gap not working

--- app/Articles.tsx
+++ app/Articles.tsx
@@ -6,7 +6,7 @@ import { BigArticle } from "./BigArticle";
 export function Articles({ articles }: { articles: readonly ArticleData[]; }) {
   return (
     <div
-      className="w-full pt-4 pr-5 pb-6 pl-5 mt-0 mr-auto mb-0 ml-auto gap-y-5 sm:py-8 md:py-12 sm:gap-y-8 md:gap-y-16
+      className="w-full pt-4 pr-5 pb-6 pl-5 mt-0 grid mr-auto mb-0 ml-auto gap-y-5 sm:py-8 md:py-12 sm:gap-y-8 md:gap-y-16
 max-w-7xl"
     >
       <BigArticle article={articles[0]}></BigArticle>

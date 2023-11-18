a7bed5608cb0fa16629a6e67c360deaeea8c2333
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-11-18 12:47:51 +0100

    feat(Articles): remove tailwindcss

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -4,11 +4,10 @@ import { BigArticle } from "./BigArticle";
 export function Articles({ articles }: { articles: readonly ArticleData[] }) {
   return (
     <div
-      className="w-full pt-4 pb-6 grid me-auto ms-auto gap-y-5 sm:py-8 md:py-12 sm:gap-y-8 md:gap-y-16
-max-w-7xl"
+      className="articles"
     >
       {articles[0] && <BigArticle article={articles[0]}></BigArticle>}
-      <p className="hidden first:block col-span-full">No articles</p>
+      <p className="articles__empty">No articles</p>
 
       <div className="grid grid-cols-12 gap-x-8 gap-y-16">
         {articles.slice(1).map((article) => (

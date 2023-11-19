2dcd3c6767b4c65532827e51c05b1c227d107803
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-11-19 12:39:59 +0100

    feat(Header): remove tailwindcss (#20) (#21)
    * feat(Chip): remove tailwindcss (#20)
    * feat(Chip): remove tailwindcss
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * chore
    
    * chore

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,36 +3,36 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import "./Header.css";
+
 export function Header({ children }: { children: React.ReactNode }) {
   const [open, setOpen] = useState(false);
 
   return (
     <nav className="">
       <div className="">
-        <div className="flex flex-wrap items-center justify-between py-4">
-          <Link href="/" className="flex items-center">
+        <div className="header">
+          <Link href="/" className="header__link">
             <Image
               width={32}
               height={32}
               src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8 me-3"
+              className="header__logo"
               alt="Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">
-              Top Blog
-            </span>
+            <span className="header__title">Top Blog</span>
           </Link>
           <button
             onClick={() => setOpen(!open)}
             data-collapse-toggle="navbar-solid-bg"
             type="button"
-            className="inline-flex items-center p-2 ms-3 text-sm rounded-lg md:hidden hover:bg-primary-container focus:outline-none focus:ring-2 focus:ring-on-primary-container text-on-primary-container"
+            className="header__button"
             aria-controls="navbar-solid-bg"
             aria-expanded={open}
           >
-            <span className="sr-only">Open main menu</span>
+            <span className="button__name">Open main menu</span>
             <svg
-              className="w-6 h-6"
+              className="header__svg"
               aria-hidden="true"
               fill="currentColor"
               viewBox="0 0 20 20"
@@ -46,7 +46,9 @@ export function Header({ children }: { children: React.ReactNode }) {
             </svg>
           </button>
           <div
-            className={`${open ? "" : "hidden"} w-full md:block md:w-auto`}
+            className={`${
+              open ? "" : "header__container--hidden "
+            }header__container`}
             id="navbar-solid-bg"
           >
             {children}

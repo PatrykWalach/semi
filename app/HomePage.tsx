79526d2eb4cc7f942033c578710e87c946d4cfaf
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-11-19 15:08:37 +0100

    feat(HomePage): remove tailwindcss (#26)
    * feat(HomePage): remove tailwindcss
    
    * fix

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,15 +1,23 @@
 import { ArticleData } from "@/components/Article";
 import { Articles } from "@/components/Articles";
 
+import "./HomePage.css";
+
 export default function HomePage({
   articles,
 }: {
   articles: readonly ArticleData[];
 }) {
   return (
-    <main className="divide-y">
-      <Articles articles={articles.slice(0, 4)}></Articles>
-      <Articles articles={articles.slice(4)}></Articles>
+    <main className="home-page divide-y">
+      {Array.from({ length: Math.ceil(articles.length / 4) }, (_, i) => {
+        return (
+          <Articles
+            key={i}
+            articles={articles.slice(i * 4, (i + 1) * 4)}
+          ></Articles>
+        );
+      })}
     </main>
   );
 }

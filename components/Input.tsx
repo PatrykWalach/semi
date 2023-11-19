8606511e43880fb8c856e550ecd9e60adb2a8b67
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-11-19 14:21:29 +0100

    feat(Input): remove tailwindcss (#22)
    * feat(Input): remove tailwindcss
    
    * fix

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,23 +1,19 @@
-
 import { ComponentPropsWithoutRef } from "react";
 
-export const className =
-  "rounded-[1.25rem] bg-inverse-surface px-3 py-2 has-[input:focus]:ring has-[[aria-invalid=true]]:ring has-[[aria-invalid=true]]:ring-error ring-inverse-primary has-[:disabled]:hover:ring-0 hover:ring text-inverse-on-surface flex";
+import "./Input.css";
 
 export default function Input({
   children,
   ...props
 }: ComponentPropsWithoutRef<"input">) {
   return (
-    <div className={className}>
+    <div className="input">
       <input
         type="text"
         {...props}
-        className={`bg-transparent disabled:opacity-50 flex-1 border-0 caret-inverse-on-surface outline-none focus:!ring-0 ${props.className}`}
+        className={`input__input ${props.className}`}
       />
       {children}
     </div>
   );
 }
-
-

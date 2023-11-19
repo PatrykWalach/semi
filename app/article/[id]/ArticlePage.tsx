5417a355f64019eb3688f2cd3d2f7a8bd6f2198c
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-11-19 15:08:05 +0100

    feat(ArticlePage): remove tailwindcss

--- app/article/[id]/ArticlePage.tsx
+++ app/article/[id]/ArticlePage.tsx
@@ -3,8 +3,8 @@ import { BigArticle } from "@/components/BigArticle";
 
 export default function ArticlePage({ article }: { article: ArticleData }) {
   return (
-    <main className="bg-surface text-on-surface">
-      <div className="container mx-auto py-4 grid gap-8">
+    <main className="article-page">
+      <div className="article-page__container">
         <header>
           <BigArticle article={article}></BigArticle>
         </header>
@@ -12,7 +12,7 @@ export default function ArticlePage({ article }: { article: ArticleData }) {
           dangerouslySetInnerHTML={{
             __html: article.content,
           }}
-          className="prose py-4 lg:prose-2xl dark:prose-invert"
+          className="prose article-page__prose lg:prose-2xl dark:prose-invert"
         ></section>{" "}
       </div>
     </main>

696c2b4ae0e314f0889dce668aded48d7bb0715d
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-01-07 14:02:48 +0100

    revert

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-import StylexPlugin from '@stylexjs/webpack-plugin';
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+ 
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -23,25 +24,6 @@ const config: StorybookConfig = {
   },
   webpackFinal: async (config, options) => {
     const plugins: any[] = [
-      new StylexPlugin({
-        filename: "styles.[contenthash].css",
-        // get webpack mode and set value for dev
-        dev: true,
-        // Use statically generated CSS files and not runtime injected CSS.
-        // Even in development.
-        // @ts-ignore
-        runtimeInjection: false,
-        // optional. default: 'x'
-        classNamePrefix: "x",
-        // Required for CSS variable support
-        unstable_moduleResolution: {
-          // type: 'commonJS' | 'haste'
-          // default: 'commonJS'
-          type: "commonJS",
-          // The absolute path to the root directory of your project
-          rootDir: __dirname,
-        },
-      }),
       ...(config.resolve?.plugins || []),
       new TsconfigPathsPlugin({
         extensions: config.resolve?.extensions,
